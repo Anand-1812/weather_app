7a4ae7c6e8e6e7c7d030e74859e612d0804ed332
Anand-1812 <anand.work2005@gmail.com>
2025-06-12 18:17:40 +0530

    add the loading animation

--- src/index.js
+++ src/index.js
@@ -8,9 +8,21 @@ const weather = new Weather();
 
 const weatherBtn = document.querySelector("button");
 const location = document.querySelector("input");
+const loader = document.getElementById("loader");
 
 weatherBtn.addEventListener("click", async () => {
-  const data = await weather.getWeather(location.value);
+  
+  loader.style.display = "block"; // show loader
+
+  const data = await weather.getWeather(location.value.trim());
   console.log(data);
-  putDataInPage(data);
+
+  if (data) {
+    setTimeout(() => {
+      loader.style.display = "none"; // hide loader after delay
+      putDataInPage(data);
+    }, 2000);
+  } else {
+    loader.style.display = "none"; // hide loader immediately if no data
+  }
 });
