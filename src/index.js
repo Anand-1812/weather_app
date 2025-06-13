983f12916b675b76e5b919041cdd40d994c81c95
Anand-1812 <anand.work2005@gmail.com>
2025-06-13 15:23:41 +0530

    improvement in data

--- src/index.js
+++ src/index.js
@@ -9,19 +9,32 @@ const weather = new Weather();
 const weatherBtn = document.querySelector("button");
 const location = document.querySelector("input");
 const spinner = document.getElementById("spinner");
+const dataDiv = document.querySelector(".data-div");
 
 weatherBtn.addEventListener("click", async () => {
+  dataDiv.innerHTML = "";
+
 
   const locationVal = location.value.trim();
   if (!locationVal) return;
 
   spinner.style.display = "block"; // Show spinner
 
-  const data = await weather.getWeather(locationVal);
+  try {
+    const data = await weather.getWeather(locationVal);
 
-  setTimeout(() => {
-    spinner.style.display = "none"; // Hide spinner
-    if (data) putDataInPage(data);
-  }, 2000);
+    setTimeout(() => {
+      spinner.style.display = "none"; // Hide spinner
+      if (data) {
+        putDataInPage(data);
+      } else {
+        dataDiv.innerHTML = "<p>No data found for this location.</p>";
+      }
+    }, 2000);
+  } catch (error) {
+    spinner.style.display = "none";
+    console.error("Failed to fetch weather data:", error);
+    dataDiv.innerHTML = "<p>Error fetching data. Please try again.</p>";
+  }
   
 });
--- src/ui.js
+++ src/ui.js
@@ -1,24 +1,24 @@
 export function putDataInPage(data) {
-  const body = document.querySelector("body");
+  const dataDiv = document.querySelector(".data-div");
 
   const weatherCard = document.createElement("div");
   weatherCard.className = "weather-data";
 
-  const city = document.createElement("p");
+  const city = document.createElement("h3");
   city.textContent = `City: ${data.city}`;
 
   const temp = document.createElement("p");
-  temp.textContent = `Temperature: ${data.temperature}`;
+  temp.textContent = `Temperature: ${data.temperature} °F`;
 
   const descp = document.createElement("p");
   descp.textContent = `Condtion: ${data.description}`;
 
   const realTemp = document.createElement("p");
-  realTemp.textContent = `Feels Like: ${data.realTemp}`;
+  realTemp.textContent = `Feels Like: ${data.realTemp} °F`;
 
   const humidity = document.createElement("p");
-  humidity.textContent = `Humidity: ${data.humid}`;
+  humidity.textContent = `Humidity: ${data.humid}%`;
 
   weatherCard.append(city, temp, descp, realTemp, humidity);
-  body.appendChild(weatherCard);
+  dataDiv.appendChild(weatherCard);
 }
